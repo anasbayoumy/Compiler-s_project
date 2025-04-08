a427a0ff562981a4a8142f1290e989481748ff88
Anas Bayoumy <167243832+anasbayoumy@users.noreply.github.com>
2025-04-08 15:46:01 +0200

    init

---
+++ project/src/lexer/lexer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello, World!" << endl;
+   cout << "This is a simple lexer." << endl;   
+
+    return 0;
+}
